209cb07560fae9e648212f24dd49571c61621885
VaibhavTanjiro123 <veeramallasaivaibhav@gmail.com>
2026-06-05 14:36:03 +0000

    FWP.PY

---
+++ FWP.py
@@ -0,0 +1,33 @@
+print('hello world how are you')
+
+username=input('What is your name?')
+
+print('Hello',username,'How are you?',sep=" ")
+
+print('Here is something cool👇')
+
+print('PPPPP','Y   Y','TTTTT','H   H','OOOOO','N   N',sep='  ')
+
+print('P   P',' Y Y  ',' T  ','H   H','O   O','NN  N',sep='  ')
+
+print('PPPPP','  Y ','   T  ','HHHHH','O   O','N N N',sep='  ')
+
+print('P    ','  Y ','   T  ','H   H','O   O','N  NN',sep='  ')
+
+print('P    ','  Y ','   T  ','H   H','OOOOO','N   N',sep='  ',end =' ')
+
+print('is soo very cool (Like meeee)')
+
+print(' ')
+
+print(' ')
+
+print('MY NAME IS :')
+
+print(' ')
+
+print('V V',' A ',' III ',' BBB ',' H H ','  A  ',' V V' )
+
+print('V V','AAA','  I  ',' BBB ',' HHH ',' AAA ',' V V' )
+
+print(' V ','A A',' III ',' BBB ',' H H ',' A A  ',' V  ')
