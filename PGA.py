0dd7471ae58817ba1ac44af79d15c43080cf7310
VaibhavTanjiro123 <veeramallasaivaibhav@gmail.com>
2026-06-27 10:09:42 +0000

    Done

--- PGA.py
+++ PGA.py
@@ -34,3 +34,38 @@ if len(Word)== 8:
 
 else:
     print('You Are A Disaster.')
+
+print()
+
+sentence= input('Tell me a sentece ending in wise assistant(no questions)')
+
+if sentence.endswith('wise assistant'):
+    print('Have you not heard about puncuations? A 1st Grader can do better...')
+elif sentence.endswith('wise assistant.'):
+    print('OK...But wait...')
+    len1st=sentence.find(' ')
+    if len1st > 8:
+        print('Your first word is too short...')
+    elif len1st==8:
+        print('Longer first word, still too short...')
+    else:
+        print('your first word is too long.')
+else:
+    print('The prof will hate you...')
+
+
+
+print()
+print('OK, pick a timing next Friday')
+print('A. 18 minutes after midnight', 'B. 23 minutes past 6 AM', sep='\t\t')
+print('C. 2 minutes past high noon', 'D. 2 seconds past 9 PM', sep='\t\t')
+appt=input("Select a slot...(A/B/C/D) You Don't Deserve This Though...")
+
+if appt == 'A':
+    print('Beware, Prof may be sleepy')
+elif appt == 'B':
+    print('Careful, Prof may be jogging')
+elif appt == 'C':
+    print('Warning, Prof may be reading')
+else:
+    print('Caution, Prof may be eating dinner') 
