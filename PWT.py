1d31ca84193cb390b1c263c6e656dd6306198883
VaibhavTanjiro123 <veeramallasaivaibhav@gmail.com>
2026-07-11 09:06:38 +0000

    yesss

--- PWT.py
+++ PWT.py
@@ -1,28 +1,65 @@
 import turtle
 
+import time
 t = turtle.Turtle()
+ts = turtle.Screen()
 
-t.shape('classic')
-t.pencolor('HotPink')
-t.fillcolor("olive drab")
-t.penup()
-t.speed(1)
-t.goto(0,100)
-t.pendown()
-t.pensize(4)
-t.begin_fill()
+ts.bgcolor("lightblue")
 
-t.circle(-100)
-t.end_fill()
-t.penup()
-t.goto(34.2, 93.9)
-t.pendown()
-t.goto(200, 0)
-t.goto(34.2, -93.9)
+while True:
+    turtle.tracer(0)
+
+    t.shape('classic')
+    t.pencolor('lightblue')
+    t.fillcolor("gold")
+    t.penup()
+    t.speed(0)
+    t.goto(0,100)
+    t.pendown()
+    t.pensize(4)
+    t.begin_fill()
 
 
+    t.circle(-100)
+    t.end_fill()
 
-t.penup()
+    t.fillcolor("silver")
+    t.penup()
+    t.begin_fill()
+    t.goto(93.9, 34.2)
+    t.pendown()
+    t.goto(200, 50)
+    t.goto(76, 64)
+    t.goto(93.9, 34.2)
+    t.end_fill()
+
+    t.penup()
+    t.begin_fill()
+    t.goto(-93.9, 34.2)
+    t.pendown()
+    t.goto(-200, 50)
+    t.goto(-76, 64)
+    t.goto(-93.9, 34.2)
+    t.end_fill()
+    t.penup()
+
+    t.color("black")
+
+    t.goto(-100, 0)
+    t.pendown()
+    t.goto(100, 0)
+    t.penup()
+    t.goto(-40, -15)
+    t.write('I open at the close', ("Lucida Handwriting", 48, "normal"))
+
+    turtle.tracer(0)
+
+    t.hideturtle()
+        
+    ts.update()
+
+
+"""t.penup()
 t.goto(0,30)
 t.pendown()
 t.setheading(90)
@@ -42,6 +79,7 @@ t.pencolor('violet')
 t.pensize(8)
 t.pendown()
 t.setheading(-90)
-t.circle(-35, 180)
+t.circle(-35, 180)"""
+
 t.hideturtle()
 turtle.mainloop()
