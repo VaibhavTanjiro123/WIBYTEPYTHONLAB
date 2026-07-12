4fecb7c6e609396786396216c39a414cbf9cf57b
VaibhavTanjiro123 <veeramallasaivaibhav@gmail.com>
2026-07-12 05:49:47 +0000

    Finally done!!!!

--- PWT.py
+++ PWT.py
@@ -3,7 +3,9 @@ t = turtle.Turtle()
 ts=t.getscreen()
 
 
+
 def draw_smiley(offset):
+    ts.tracer(0)
     t.penup()
     t.pencolor('HotPink')
     t.fillcolor('firebrick1')
@@ -41,10 +43,24 @@ def draw_smiley(offset):
     t.goto(35, -20)
     t.end_fill()
 
-draw_smiley(0)
-
-
 ts.update()
 
+offset=0
+def animate_drawing():
+    global offset
+    if offset ==0:
+        offset = 15
+    else:
+        offset = 0
+    draw_smiley(offset)
+    ts.ontimer(animate_drawing,500)
+
+
+animate_drawing()
+t.penup()
+t.goto(-200,110)
+t.write("Hello,My name is Happyman!", font=("Arial", 24, "normal")) 
+t.hideturtle()
+ts.update()
 
 turtle.mainloop()
